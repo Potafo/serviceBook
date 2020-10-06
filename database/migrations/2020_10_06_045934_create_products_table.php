96e4aa3eb09a44ed3021ca910fb1b91431d63211
jeshina-potafo <67554907+jeshina-potafo@users.noreply.github.com>
2020-10-06 11:15:03 +0400

    migration update

--- database/migrations/2020_10_06_045934_create_products_table.php
+++ database/migrations/2020_10_06_045934_create_products_table.php
@@ -18,7 +18,7 @@ class CreateProductsTable extends Migration {
 			$table->integer('id', true);
 			$table->string('name', 200);
 			$table->string('image', 200);
-			$table->integer('vendor_id')->index('vendor_product_id');
+			$table->integer('vendor_id')->unsigned()->index('vendor_product_id');
 			$table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
 		});
