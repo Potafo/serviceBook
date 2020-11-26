d91e450041072c504137c4cf7e4202f43a552df9
jeshina-potafo <67554907+jeshina-potafo@users.noreply.github.com>
2020-11-26 20:19:00 +0400

    service js

--- resources/views/services/services_add.blade.php
+++ resources/views/services/services_add.blade.php
@@ -103,7 +103,7 @@ select > option {
     </div>
 @endsection
 {{-- <script src="{{ asset('black') }}/js/jquery.min.js"></script> --}}
-<script src="{{ asset('black') }}/js/core/jquery-2.1.3.min.js"></script>
+<script src="{{ asset('black') }}/js/core/jquery-3.4.1.min.js"></script>
 <script language="JavaScript" type="text/javascript">
  $(document).ready(function() {
 
