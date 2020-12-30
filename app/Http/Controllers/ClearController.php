ecec65215fd05a2c36607673a640d609c7f2ec2f
jeshina-potafo <67554907+jeshina-potafo@users.noreply.github.com>
2020-12-30 15:25:09 +0400

    update 3

--- app/Http/Controllers/ClearController.php
+++ app/Http/Controllers/ClearController.php
@@ -10,7 +10,7 @@ class ClearController extends Controller
 {
     public function cleartables($tables)
     {
-        DB::select("ALTER TABLE service_book.status_change_history DROP FOREIGN KEY fk_statuschangehistory_cart_jobcardnumber");
+        //DB::select("ALTER TABLE service_book.status_change_history DROP FOREIGN KEY fk_statuschangehistory_cart_jobcardnumber");
         $vendor=DB::delete("TRUNCATE TABLE $tables ");
         //$vendor=DB::delete("TRUNCATE TABLE `job_card`");
         //DB::raw("TRUNCATE TABLE `job_card`");
