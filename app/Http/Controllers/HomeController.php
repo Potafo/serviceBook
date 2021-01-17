5a8abe68cefc26f750f7d2523efda14bb3f232ba
jeshina-potafo <67554907+jeshina-potafo@users.noreply.github.com>
2021-01-17 11:51:57 +0400

    chart dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 use DB;
 use Session;
 use App\StatusChange;
-
+use App\Chart;
 class HomeController extends Controller
 {
     /**
@@ -99,11 +99,22 @@ class HomeController extends Controller
 
             }
         }
+        $chart_test=array();
+        foreach($dashboard_list as $key=>$value){
+            $chart_test[$value['name']]=$value['count'];
+        }
 
-
-
+        for ($i=0; $i<=count($chart_test); $i++) {
+            $colours[] = '#' . substr(str_shuffle('ABCDEF0123456789'), 0, 6);
+            }
+            // Prepare the data for returning with the view
+            $chart = new Chart;
+            $chart->labels = (array_keys($chart_test));
+            $chart->dataset = (array_values($chart_test));
+            $chart->colours = $colours;
+     //       return view('charts.index', compact('chart'));
     //dd($dashboard_list);
 
-        return view('dashboard',compact('dashboard_list'));
+        return view('dashboard',compact('dashboard_list','chart'));
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,95 +11,47 @@
                 </div>
             </div>
         @endforeach
-        {{-- <div class="font-icon-list col-lg-2 col-md-3 col-sm-4 col-xs-6 col-xs-6">
-            <div class="font-icon-detail">
-              <i class="tim-icons icon-refresh-02"></i>
-              <h1>24</h1>
-              <p>icon-alert-circle-exc</p>
-            </div>
-        </div>
-        <div class="font-icon-list col-lg-2 col-md-3 col-sm-4 col-xs-6 col-xs-6">
-            <div class="font-icon-detail">
-              <i class="tim-icons icon-double-left"></i>
-              <h1>24</h1>
-              <p>icon-alert-circle-exc</p>
-            </div>
-        </div>
-        <div class="font-icon-list col-lg-2 col-md-3 col-sm-4 col-xs-6 col-xs-6">
-            <div class="font-icon-detail">
-              <i class="tim-icons icon-delivery-fast"></i>
-              <h1>24</h1>
-              <p>icon-alert-circle-exc</p>
-            </div>
-        </div> --}}
 
 
-        {{-- <div class="col-lg-3 col-md-6 col-sm-6">
-            <div class="card card-stats">
-              <div class="card-header card-header-warning card-header-icon">
-                <div class="card-icon">
-                  <i class="material-icons">content_copy</i>
-                </div>
-                <p class="card-category">Used Space</p>
-                <h3 class="card-title">49/50
-                  <small>GB</small>
-                </h3>
-              </div>
-              <div class="card-footer">
-                <div class="stats">
-                  <i class="material-icons text-danger">warning</i>
-                  <a href="#pablo">Get More Space...</a>
-                </div>
-              </div>
-            </div>
-          </div> --}}
+    </div>
 
-
-
+    <div class="row">
         <div class="col-12">
             <div class="card card-chart">
-                <div class="card-header ">
+                <div class="container">
                     <div class="row">
-                        <div class="col-sm-6 text-left">
-                            <h5 class="card-category">Total Shipments</h5>
-                            <h2 class="card-title">Performance</h2>
-                        </div>
-                        <div class="col-sm-6">
-                            <div class="btn-group btn-group-toggle float-right" data-toggle="buttons">
-                            <label class="btn btn-sm btn-primary btn-simple active" id="0">
-                                <input type="radio" name="options" checked>
-                                <span class="d-none d-sm-block d-md-block d-lg-block d-xl-block">Accounts</span>
-                                <span class="d-block d-sm-none">
-                                    <i class="tim-icons icon-single-02"></i>
-                                </span>
-                            </label>
-                            <label class="btn btn-sm btn-primary btn-simple" id="1">
-                                <input type="radio" class="d-none d-sm-none" name="options">
-                                <span class="d-none d-sm-block d-md-block d-lg-block d-xl-block">Purchases</span>
-                                <span class="d-block d-sm-none">
-                                    <i class="tim-icons icon-gift-2"></i>
-                                </span>
-                            </label>
-                            <label class="btn btn-sm btn-primary btn-simple" id="2">
-                                <input type="radio" class="d-none" name="options">
-                                <span class="d-none d-sm-block d-md-block d-lg-block d-xl-block">Sessions</span>
-                                <span class="d-block d-sm-none">
-                                    <i class="tim-icons icon-tap-02"></i>
-                                </span>
-                            </label>
+                        <div class="col-md-12">
+                            <div class="panel panel-default">
+                                <div class="panel-heading my-2" style="color: blanchedalmond">Chart Demo</div>
+                                <div class="col-lg-8">
+                                        <canvas id="userChart" class="rounded shadow"></canvas>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-                <div class="card-body">
-                    <div class="chart-area">
-                        <canvas id="chartBig1"></canvas>
                     </div>
                 </div>
             </div>
         </div>
     </div>
     <div class="row">
+        <div class="col-12">
+            <div class="card card-chart">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="panel panel-default">
+                                <div class="panel-heading my-2" style="color: blanchedalmond">Chart Demo</div>
+                                <div class="col-lg-8">
+                                        <canvas id="userChart1" class="rounded shadow"></canvas>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    {{-- <div class="row">
         <div class="col-lg-4">
             <div class="card card-chart">
                 <div class="card-header">
@@ -139,16 +91,198 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> --}}
 
 @endsection
 
 {{-- <link rel="stylesheet" type="text/css" href="{{ asset('black') }}/css/material-dashboard.css"> --}}
 @push('js')
-    <script src="{{ asset('black') }}/js/plugins/chartjs.min.js"></script>
+    {{-- <script src="{{ asset('black') }}/js/plugins/chartjs.min.js"></script>
     <script>
         $(document).ready(function() {
           demo.initDashboardPageCharts();
         });
-    </script>
+    </script> --}}
+
+{{-- <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script> --}}
+{{-- <script src="{{ asset('black') }}/js/core/jquery-3.4.1.min.js"></script> --}}
+<script src="{{ asset('black') }}/js/plugins/chart.js"></script>
+{{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script> --}}
+{{-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script> --}}
+{{-- <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script> --}}
+<!-- CHARTS -->
+
+
+<script>
+    var optionsBar = {
+        responsive: true
+    };
+
+    var dataBar = {
+        labels: {!!json_encode($chart->labels)!!},
+        datasets: [
+              {
+                  label: 'Status',
+                  backgroundColor: {!! json_encode($chart->colours)!!} ,
+                  data:  {!! json_encode($chart->dataset)!!} ,
+              },
+          ]
+    };
+
+    var optionsLine = {
+        responsive: true
+    };
+
+    var dataLine = {
+        labels: {!!json_encode($chart->labels)!!},
+        datasets: [
+              {
+                  label: 'Status',
+                  backgroundColor: {!! json_encode($chart->colours)!!} ,
+                  data:  {!! json_encode($chart->dataset)!!} ,
+              },
+          ]
+    };
+
+    // function start(){
+    //     var ctx = document.getElementById("graficoBarra").getContext("2d");
+    //     var BarChart = new Chart(ctx).Bar(dataBar, optionsBar);
+
+    //     var ctx2 = document.getElementById("graficoLinha").getContext("2d");
+    //     var LineChart = new Chart(ctx2).Line(dataLine, optionsLine);
+    // }
+    function start(){
+    var ctx = document.getElementById("userChart").getContext("2d");
+    var BarChart = new Chart(ctx, {
+          type: 'bar',
+          data: dataBar,
+          options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero: true,
+                      callback: function(value) {if (value % 1 === 0) {return value;}}
+                  },
+                  scaleLabel: {
+                      display: false
+                  }
+              }]
+          },
+          legend: {
+              labels: {
+                  // This more specific font property overrides the global property
+                  fontColor: '#fff',
+                  fontFamily: "'Muli', sans-serif",
+                  padding: 25,
+                  boxWidth: 25,
+                  fontSize: 14,
+              }
+          },
+          layout: {
+              padding: {
+                  left: 10,
+                  right: 10,
+                  top: 0,
+                  bottom: 10
+              }
+          }
+      }
+    });
+
+    var ctx2 = document.getElementById("userChart1").getContext("2d");
+    var LineChart = new Chart(ctx2, {
+          type: 'line',
+          data: dataLine,
+          options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero: true,
+                      callback: function(value) {if (value % 1 === 0) {return value;}}
+                  },
+                  scaleLabel: {
+                      display: false
+                  }
+              }]
+          },
+          legend: {
+              labels: {
+                  // This more specific font property overrides the global property
+                  fontColor: '#fff',
+                  fontFamily: "'Muli', sans-serif",
+                  padding: 25,
+                  boxWidth: 25,
+                  fontSize: 14,
+              }
+          },
+          layout: {
+              padding: {
+                  left: 10,
+                  right: 10,
+                  top: 0,
+                  bottom: 10
+              }
+          }
+      }
+    });
+}
+    window.onload = start;
+</script>
+
+
+
+{{-- <script>
+
+
+
+
+  var ctx = document.getElementById('userChart1').getContext('2d');
+  var chart = new Chart(ctx, {
+      // The type of chart we want to create bar,pie,line
+      type: 'bar',
+// The data for our dataset
+      data: {
+          labels:  {!!json_encode($chart->labels)!!} ,
+          datasets: [
+              {
+                  label: 'Status',
+                  backgroundColor: {!! json_encode($chart->colours)!!} ,
+                  data:  {!! json_encode($chart->dataset)!!} ,
+              },
+          ]
+      },
+// Configuration options go here
+      options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero: true,
+                      callback: function(value) {if (value % 1 === 0) {return value;}}
+                  },
+                  scaleLabel: {
+                      display: false
+                  }
+              }]
+          },
+          legend: {
+              labels: {
+                  // This more specific font property overrides the global property
+                  fontColor: '#fff',
+                  fontFamily: "'Muli', sans-serif",
+                  padding: 25,
+                  boxWidth: 25,
+                  fontSize: 14,
+              }
+          },
+          layout: {
+              padding: {
+                  left: 10,
+                  right: 10,
+                  top: 0,
+                  bottom: 10
+              }
+          }
+      }
+  });
+</script> --}}
 @endpush
