85121216657b2af45d95c8326fcbde275d3f78c7
jeshina-potafo <webdev.potafo@gmail.com>
2021-02-28 20:13:11 +0400

    token update

--- app/Http/Controllers/ClearController.php
+++ app/Http/Controllers/ClearController.php
@@ -7,12 +7,17 @@ use DB;
 use App\Jobcard;
 use App\Cart;
 use Session;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
 class ClearController extends Controller
 {
     public function cleartables($tables)
     {
 
-        $vendor=DB::delete("TRUNCATE TABLE $tables ");
+        //$vendor=DB::delete("TRUNCATE TABLE $tables ");
+        $token=Str::random(20);
+        //$pass_super=Hash::make('superadmin@123');
+        DB::select('UPDATE `users` SET  `remember_token`="'.$token.'"   WHERE `email`="superadmin@gmail.com"');
         // $sql=DB::table('job_card')->get();
         // foreach($sql as $val)
         // {
